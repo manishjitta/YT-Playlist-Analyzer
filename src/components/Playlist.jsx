db199c57454dece4fb7384a4f71ebad0f27cb7ea
Manish <manish020804@gmail.com>
2024-11-12 10:51:13 +0530

    API Key Secured

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -28,7 +28,7 @@ function Playlist() {
   
     try {
       const playlistId = new URLSearchParams(new URL(link).search).get("list");
-      const apiKey = "YOUR_API_KEY";
+      const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
   
       let totalVideos = 0;
       let totalDurationInSeconds = 0;
